3e09f4f453ff9576e2602e62ea575a500355e987
junghong <junghong@ninewatt.com>
2021-12-22 11:50:44 +0900

    #2.7 Themes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import styled, { keyframes } from "styled-components";
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
 const Wrapper = styled.div`
   display: flex;
   width: 100vw;
   height: 100vh;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 const rotationAnimation = keyframes`
@@ -47,6 +52,7 @@ const Box = styled.div`
 function App() {
   return (
     <Wrapper>
+      <Title>TEST </Title>
       <Box>
         <Emogi as="p">🧷</Emogi>
       </Box>
