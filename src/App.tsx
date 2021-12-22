b3959abae31f412c50541237287e5764ed103565
junghong <junghong@ninewatt.com>
2021-12-22 18:00:34 +0900

    #3.2 Typing the Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,12 @@
-import styled, { keyframes } from "styled-components";
-
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  width: 100vw;
-  height: 100vh;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
-
-const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emogi = styled.span`
-  font-size: 50px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotationAnimation} 5s linear infinite;
-  ${Emogi} {
-    &:hover {
-      font-size: 100px;
-    }
-    &:active {
-      opacity: 0.5;
-    }
-  }
-`;
+import styled from "styled-components";
+import Circle from "./Circle";
 
 function App() {
   return (
-    <Wrapper>
-      <Title>TEST </Title>
-      <Box>
-        <Emogi as="p">🧷</Emogi>
-      </Box>
-    </Wrapper>
+    <div>
+      <Circle bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </div>
   );
 }
 
