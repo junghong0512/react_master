582355eeddf30b900355437d068417c1d3da6847
junghong <junghong@ninewatt.com>
2021-12-23 10:56:51 +0900

    #3.6 Themes

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,18 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 function App() {
+  const Container = styled.div`
+    background-color: ${props => props.theme.bgColor};
+  `;
 
-  const [value, setValue] = useState("");
-
-  const onChange = (event: React.FormEvent<HTMLInputElement>) =>  {
-    const { 
-      currentTarget: { value } 
-    } = event;
-    setValue(value);
-  }
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello: ", value)
-  }
+  const H1 = styled.h1`
+    color: ${props => props.theme.textColor};
+  `;
   
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input 
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username" 
-        />
-        <button>Log In</button>
-      </form>
-    </div>
+    <Container>
+        <H1>Proptected</H1>
+    </Container>
   );
 }
 
