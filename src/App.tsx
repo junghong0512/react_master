26d446e3d86f9dc7b67d1e7c960377d7be0b99bf
junghong <junghong@ninewatt.com>
2022-01-07 10:41:29 +0900

    #7.9 MotionValues Part Three

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { DragDropContext, DropResult } from "react-beautiful-dnd";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform } from "framer-motion";
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  useViewportScroll,
+} from "framer-motion";
 import { useEffect, useRef } from "react";
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
+  background-color: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
 `;
 
 const BiggerBox = styled.div`
@@ -41,16 +47,36 @@ function App() {
   const x = useMotionValue(0);
   const customOpacity = useTransform(x, [-800, 0, 800], [1, 0.5, 0.01]);
   const customScale = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
+  const customRotate = useTransform(x, [-800, 800], [-360, 360]);
+  const customGradient = useTransform(
+    x,
+    [-800, 800],
+    [
+      "linear-gradient(135deg, rgb(0, 238, 238), rgb(89, 170, 159))",
+      "linear-gradient(135deg, rgb(120, 110, 160), rgb(23, 109, 180))",
+    ]
+  );
+
+  const { scrollY, scrollYProgress } = useViewportScroll();
+  const scrollScale = useTransform(scrollYProgress, [0, 1], [1, 5]);
 
   useEffect(() => {
     // x.onChange(() => console.log(x.get()));
-    customScale.onChange(() => console.log(customScale.get()));
-  }, [x]);
+    // customScale.onChange(() => console.log(customScale.get()));
+    scrollY.onChange(() => console.log(scrollY.get(), scrollYProgress.get()));
+  }, [scrollY, scrollYProgress]);
 
   return (
-    <Wrapper>
+    <Wrapper style={{ background: customGradient }}>
       <Box
-        style={{ x, scale: customScale, opacity: customOpacity }}
+        style={{
+          x,
+          opacity: customOpacity,
+          rotateZ: customRotate,
+          // scale: customScale,
+          // scale: scrollYProgress,
+          scale: scrollScale,
+        }}
         drag="x"
         dragSnapToOrigin
       />
