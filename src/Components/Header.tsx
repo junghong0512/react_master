41924bb225d54cc6d029be4869642f59fe167427
junghong <junghong@ninewatt.com>
2022-01-10 13:22:05 +0900

    #8.6 Home Screen Part Two

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -174,14 +174,15 @@ function Header() {
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <motion.path
+            <path
               fillRule="evenodd"
               d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
               clipRule="evenodd"
-            ></motion.path>
+            ></path>
           </motion.svg>
           <Input
             animate={inputAnimation}
+            initial={{ scaleX: 0 }}
             // animate={{ scaleX: searchOpen ? 1 : 0 }} // this codes triggered in toggleSearch function
             transition={{ type: "linear" }}
             placeholder="Search for movie or tv show..."
--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,22 @@
 const API_KEY = "965b1cb9d4f8aa5a05827a33693623c1";
 const BASE_PATH = "https://api.themoviedb.org/3";
 
+export interface IMovie {
+    id: number;
+    title: string;
+    overview: string;
+    poster_path: string;
+    backdrop_path: string;
+}
+
+export interface IGetMoviesResult {
+    dates: { maximum: string, minimum:string};
+    page: number;
+    results: [IMovie];
+    total_pages: number;
+    total_results: number;
+}
+
 export function getMovies() {
     return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
         response => response.json()
